939acd450a8e8c052eb41d89b6d7866f3d48f061
tkardaris-tester <athanasios.kardaris@gmail.com>
2020-03-09 19:43:50 +0200

    "changetext"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ package main.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Demo for git");
+        System.out.println("Demo for git.");
     }
 }
