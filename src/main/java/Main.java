9805b32a6a91aa5f50368632618e77766bb211f0
tkardaris-tester <athanasios.kardaris@gmail.com>
2020-03-10 09:35:03 +0200

    upadte-main-user2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ package main.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Demo for git.");
+
+        System.out.println("Demo for git. from user 2");
+        System.out.println("This is the updated main from user 2|");
     }
 }
