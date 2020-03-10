55ff383eefb63d26fa6769619f5f4d7613977709
tkardaris-tester <athanasios.kardaris@gmail.com>
2020-03-10 09:37:36 +0200

    update-main-user2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ package main.java;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Demo for git.");
+        System.out.println("This is the updated main from user 1");
     }
 }
